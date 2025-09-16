761b973f30e50f799af17a963e89cb991b108670
Yiosko <diegoalejandrogutierrez77@gmail.com>
2025-09-16 14:16:58 -0500

    feat(Contacto): Se configura la URL de configuracion

---
+++ IceGarden/src/pages/Contacto.jsx
@@ -0,0 +1,15 @@
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+import Home   from "../layout/contacto/Home";
+
+function Contanto () {
+    return (
+        <>
+            <Header />
+            <Home />
+            <Footer />
+        </>
+    )
+}
+
+export default Contanto;
