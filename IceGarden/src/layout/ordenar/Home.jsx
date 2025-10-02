5db37574ff881a22ac9fc83ee8f4532f44fffacb
Yiosko <diegoalejandrogutierrez77@gmail.com>
2025-10-01 22:23:41 -0500

    feat(Ordenar): Ya se puede visualizar la orden y la renderizacion 3
    (Pendiente):
    
    - Realizar la cancelacion del algun producto o toppings.
    
    - Cargar los iconos de los tamaños y toppings.

--- IceGarden/src/layout/ordenar/Home.jsx
+++ IceGarden/src/layout/ordenar/Home.jsx
@@ -22,26 +22,49 @@ function Home () {
                 ...prev,
                 [idHelado]: {
                     tamaño: opcion.tamaño,
+                    topMax: opcion.topMax,
+                    freeTop: opcion.freeTop,
+                    icon: opcion.icon,
                     toppings: [],
                     valorTotal: opcion.precio
                 },
             })
         )
 
+        console.log(pedido);
         setContador((prev)=> prev + 1)
         setHelado(idHelado)
         setPasos(2)
     }
 
     const agregarToppings = (topping) => {
+        setPedido((prev) => {
+            const esHelado = prev[helado];
+            if(!esHelado) return prev;
 
+            let valor = esHelado.valorTotal
+
+            if(esHelado.toppings.length >= esHelado.topMax) return prev;
+
+            if(esHelado.freeTop <= esHelado.toppings.length){
+                valor += topping.precio;
+            }
+            return {
+                ...prev,
+                [helado]: {
+                    ...esHelado,
+                    toppings: [...esHelado.toppings, topping.name],
+                    valorTotal: valor
+                },
+            }
+        })
     }
 
     return (
         <>
             <div className="min-h-screen bg-white">
                 { pasos === 1 && ( <>
-                    <div className='flex row justify-between max-w-5xl mx-auto p-9'>
+                    <div className='flex row justify-between max-w-5xl mx-auto p-4'>
                         {
                             recipientes.map((cat) => (
                                 <button key={cat.categoria}
@@ -54,15 +77,33 @@ function Home () {
                         }
                     </div>
 
-                    <div className='flex flex-col items-center w-7xl mx-auto p-6 bg-amber-200'>
+                    <div className='flex row mx-auto items-center w-full max-w-7xl h-[14vh] bg-amber-800 rounded-2xl mb-3'>
+                        {
+                            Object.keys(pedido).map((idHelado) => {
+                                const HeladoIcon = pedido[idHelado].icon;
+                                return (
+                                    <div key={idHelado}
+                                        className='flex w-20 justify-center h-[10vh] bg-rose-400 m-2 rounded-2xl'
+                                        onClick={() => {
+                                            setHelado(idHelado);
+                                            setPasos(2);
+                                        }}>
+                                        {<HeladoIcon className="h-[10vh]" />}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
+                    <div className='flex flex-col items-center max-w-7xl mx-auto p-6 bg-amber-200'>
                         <h2 className='text-2xl font-bold mb-3'>
                             Opciones de {vista}
                         </h2>
-                        <div className='flex row justify-between w-6xl'>
+                        <div className='flex flex-col w-4 md:flex-row justify-between items-center md:w-6xl'>
                             {
                             recipientes.find((cat) => (cat.categoria === vista))
                             ?.opciones.map((opcion, idx) => (
-                                <div key={idx} className='h-96 min-w-60 card p-3'>
+                                <div key={idx} className='h-64 md:h-96 min-w-60 card p-3'>
                                     <div className='card-inner'>
                                         <div className='card-front bg-cover bg-center blur-md inset-0' style={{ backgroundImage: `url(${opcion.img})` }}>
 
@@ -102,15 +143,27 @@ function Home () {
                 </>)}
 
                 { pasos === 2 && helado && (<>
-                    <button className='bg-cyan-500'
-                            onClick={() => setPasos(1)}>
-                        Regresar
-                    </button>
                     <h2 className='bg-cyan-400'>Agregar Toppings</h2>
 
                     <div className='flex flex-col items-center md:flex-row md:max-w-7xl md:justify-between mx-auto p-6 bg-amber-600'>
                         <div className='h-[32rem] w-96 bg-amber-100'>
-                            
+                            <div className='h-full flex flex-col relative'>
+                                <span className='bg-cyan-400 w-full'>
+                                    {pedido[helado]?.tamaño.toLocaleUpperCase()}
+                                </span>
+                                    {
+                                        pedido[helado]?.toppings.map((ice, idx) => (
+                                            <span key={idx}
+                                                  className='bg-cyan-400 w-full'>
+                                                {ice.toLocaleUpperCase()}
+                                            </span>
+                                        ))
+
+                                    }
+                                <span className='bg-cyan-300 w-full absolute bottom-0'>
+                                    {pedido[helado]?.valorTotal}
+                                </span>
+                            </div>
                         </div>
 
                         <div className='h-[32rem] items-center w-full md:w-[67%] bg-amber-200'>
@@ -178,6 +231,23 @@ function Home () {
 
                             </div>
                         </div>
+                    </div>
+                    <button className='bg-cyan-500'
+                            onClick={() => setPasos(1)}>
+                        Ordenar Otro
+                    </button>
+                    <button className='bg-amber-600'
+                            onClick={() => setPasos(3)}>
+                        Continuar Pago
+                    </button>
+                </>)
+                }
+
+                {pasos === 3 && (<>
+                    <h3>Realizar Pago</h3>
+
+                    <div id="content">
+
                     </div>
                 </>)
 
