e13e5baa2e707b0cd3ce54ceb066179e589f50f0
Yiosko <diegoalejandrogutierrez77@gmail.com>
2025-10-10 22:26:21 -0500

    feat(Ordenar): Se realizo la vista de pago
    PENDIENTE realizar hook de valorPagar o SubTotal y organizar un poco más la vista para realizar la pasarela de pago
    
    -Tambien reestructurar

--- IceGarden/src/layout/ordenar/Home.jsx
+++ IceGarden/src/layout/ordenar/Home.jsx
@@ -142,11 +142,64 @@ function Home () {
                 }
 
                 {pasos === 3 && (<>
-                    <h3>Realizar Pago</h3>
 
-                    <div id="content">
+
+                    <div className='flex flex-row w-full justify-center'>
+                        <div className='flex flex-col relative rounded-2xl w-[45vw] h-[80vh] m-4'>
+                            <div className='absolute w-full h-full rounded-2xl bg-white opacity-50 -z-10'>
+                                {/* FONDO */}
+                            </div>
+
+                            {
+                                Object.keys(pedido).map((idHelado) => {
+                                    const IconHelado = pedido[idHelado].icon;
+                                    return (
+                                        <div className='flex flex-row items-center justify-center'>
+                                            <div className='flex items-center bg-white h-[12vh] w-[80%] rounded-2xl m-1'>
+                                                <span className='h-[10vh] w-[12%] ml-4 rounded-2xl bg-pink-500'>
+                                                    {IconHelado ? <IconHelado className='h-full w-full' /> : pedido[idHelado].tamaño.toLocalUpperCase()}
+                                                </span>
+                                                <span className='text-4xl text-emerald-800 font-bold'>
+                                                    $ {pedido[idHelado].valorTotal}
+                                                </span>
+                                            </div>
+                                            <button className='bg-pink-500 text-pink-200 h-[12vh] rounded-2xl w-[10%] text-4xl font-bold'
+                                                    onClick={() => {eliminarHelado(idHelado)}}>
+                                                X
+                                            </button>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className='flex flex-col items-center relative rounded-2xl w-[30vw] h-[80vh] m-4'>
+                            <div className='absolute w-full h-full rounded-2xl bg-white opacity-50 -z-10'>
+                                {/* FONDO */}
+                            </div>
+                            <span className='flex flex-row justify-center items-center bg-green-500 border-b-emerald-600 border-b-8 rounded-2xl h-[15vh] w-[92%] mt-5'>
+                                <span className='text-2xl font-bold'>
+                                    TOTAL: 
+                                </span>
+                                <span className='text-2xl font-medium pl-2'>
+                                    $ 100000
+                                </span>
+                            </span>
+
+                            <span className='flex flex-col justify-center rounded-2xl bg-white w-[92%] h-[55vh] mt-4'>
+                                Formas de pago
+                            </span>
+                        </div>
 
                     </div>
+
+                    <button className='bg-pink-600 rounded-2xl w-[35vw] h-[5vh] m-2'
+                            onClick={() => setPasos(1)}>
+                        Regresar
+                    </button>
+                    <button className='bg-emerald-700 rounded-2xl w-[35vw] h-[5vh] m-2'
+                            onClick={() => alert('Aun no esta habilitada')}>
+                        Relizar Pago
+                    </button>
                 </>)
 
                 }
