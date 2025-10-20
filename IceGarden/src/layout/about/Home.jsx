1b89490b5554fe667a6ce0fb59048df34c0427cf
Yiosko <diegoalejandrogutierrez77@gmail.com>
2025-10-20 07:17:57 -0500

    Co-authored-by: Sergio Cuervo <cuervosergioignacio@gmail.com>

--- IceGarden/src/layout/about/Home.jsx
+++ IceGarden/src/layout/about/Home.jsx
@@ -6,7 +6,9 @@ function Home () {
             
             <div className=' w-full  bg-white m-0 p-0'>
                 <div className=' relative w-full h-auto flex items-center justify-center mt-0'>
-                 <img src={burbujas} alt="Burbujas" className="w-full h-70 m-0 p-auto" />
+
+                 <img src={burbujas} alt="Burbujas" className="w-full h-full m-0 p-auto" /> {/* IMG */}
+
                  <p className="absolute text-sky-600 font-extrabold text-3xl text-center"> SOMOS LA FAMILIA DE LA PATRIA HUILENSE, DE LA SANGRE OPITA</p>
                 </div>
                 
