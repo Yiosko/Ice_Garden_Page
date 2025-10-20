e2771c747d5e82f9e1a37a9b430369cd85899cf6
SergioCuervo <cuervosergioignacio@gmail.com>
2025-10-20 07:03:34 -0500

    comienzo

--- IceGarden/src/layout/about/Home.jsx
+++ IceGarden/src/layout/about/Home.jsx
@@ -1,12 +1,22 @@
 import '../../style/App.css';
+import burbujas from '../../assets/burbujas.jpg';
 
 function Home () {
     return (
-        <>
-            <div className='min-h-screen flex flex-col md:flex-row'>
-
+            
+            <div className=' w-full  bg-white m-0 p-0'>
+                <div className=' relative w-full h-auto flex items-center justify-center mt-0'>
+                 <img src={burbujas} alt="Burbujas" className="w-full h-70 m-0 p-auto" />
+                 <p className="absolute text-sky-600 font-extrabold text-3xl text-center"> SOMOS LA FAMILIA DE LA PATRIA HUILENSE, DE LA SANGRE OPITA</p>
+                </div>
+                
+                <div className='bg-amber-50 p-10 text-center flex flex-col gap-5'>
+                    <p>Crear momentos felices para sus clientes ofreciendo helados de yogurt naturales,
+                         personalizables, en un ambiente inclusivo, creativo y respetuoso del medio ambiente.</p>
+                    <img src="" alt="" />
+                </div>
             </div>
-        </>
+            
     )
 }
 
