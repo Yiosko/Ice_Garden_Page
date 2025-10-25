ab8c7863f26ded51b88a161c50d968298c1e0114
SergioCuervo <cuervosergioignacio@gmail.com>
2025-10-25 15:23:13 -0500

    about us 1.3

--- IceGarden/src/layout/about/Home.jsx
+++ IceGarden/src/layout/about/Home.jsx
@@ -1,22 +1,24 @@
 import '../../style/App.css';
-import burbujas from '../../assets/burbujas.jpg';
+import papa from '../../assets/Icepapa.jpg';
 
 function Home () {
     return (
             
-            <div className=' w-full  bg-white m-0 p-0'>
-                <div className=' relative w-full h-auto flex items-center justify-center mt-0'>
-
-                 <img src={burbujas} alt="Burbujas" className="w-full h-full m-0 p-auto" /> {/* IMG */}
-
+            <div className='bg-blue-50 p-5 rounded-3xl shadow-black m-5 '>
+                <div className=' bg-cyan-500 rounded-3xl flex justify-center items-center shadow-lg p-10 mb-5 '>
                  <p className="absolute text-sky-600 font-extrabold text-3xl text-center"> SOMOS LA FAMILIA DE LA PATRIA HUILENSE, DE LA SANGRE OPITA</p>
-                </div>
+                </div>  
                 
-                <div className='bg-amber-50 p-10 text-center flex flex-col gap-5'>
+                <section className=' bg-amber-100 rounded-3xl shadow-lg p-5 mb-0 flex flex-col '>
                     <p>Crear momentos felices para sus clientes ofreciendo helados de yogurt naturales,
-                         personalizables, en un ambiente inclusivo, creativo y respetuoso del medio ambiente.</p>
-                    <img src="" alt="" />
-                </div>
+                        personalizables, en un ambiente inclusivo, creativo y respetuoso del medio ambiente.</p>
+                    
+                    <div className=' bg-amber-200 w-120 h-140 mx-auto md:flex-row  rounded-lg shadow-lg'>
+                    <img src={papa} alt="Icepapa" className='w-96 h-auto mx-auto my-10 md:flex-row rounded-lg shadow-lg'/>
+                    </div>
+                </section>
+                
+
             </div>
             
     )
